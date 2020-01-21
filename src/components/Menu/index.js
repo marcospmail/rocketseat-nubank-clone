5cd048d51057c14335111f1ecfc32a1018cc5460
Marcos Paulo <marcosp.mail@gmail.com>
2020-01-20 22:11:09 -0300

    feat: Implements nubank main interface

---
+++ src/components/Menu/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import QRCode from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Code, Nav, NavItem, NavText } from './styles';
+
+export default function Menu() {
+  return (
+    <Container>
+      <Code>
+        <QRCode
+          value="https://rocketseat.com.br"
+          logoSize={80}
+          color="#FFF"
+          backgroundColor="#8B10AE"
+        />
+      </Code>
+
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
+    </Container>
+  );
+}
---
+++ src/pages/Main/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Header from '~/components/Header';
+import Menu from '~/components/Menu';
+import Tabs from '~/components/Tabs';
+
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
+
+function Main() {
+  return (
+    <Container>
+      <Header />
+
+      <Content>
+        <Menu />
+
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description> R$ 197.611,65</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Marcos Paulo
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
+    </Container>
+  );
+}
+
+export default Main;
